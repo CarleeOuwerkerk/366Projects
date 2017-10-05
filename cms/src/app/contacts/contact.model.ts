81f39063ce81b37e010d2a777cc385a5a87850c0
CarleeOuwerkerk <carleeouwerkerk@shaw.ca>
2017-10-05 10:53:01 -0600

    10/05 Refactors | Added Message Component

--- cms/src/app/contacts/contact.model.ts
+++ cms/src/app/contacts/contact.model.ts
@@ -4,20 +4,20 @@ export class Contact {
   public email: string;
   public phone: string;
   public imageURL: string;
-  // public group: string[];
+  public group: string[];
 
   constructor(contactID: string
               , name: string
               , email: string
               , phone: string
               , imageURL: string
-              // , group?: string[]
+              , group?: string[]
   ){
     this.contactID = contactID;
     this.name = name;
     this.email = email;
     this.phone = phone;
     this.imageURL = imageURL;
-    // this.group = group;
+    this.group = group;
   }
 }
